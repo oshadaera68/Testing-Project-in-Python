9f24928196a0e9272e4a19acdc5d60e7068a9d70
oshadaera68 <mindartlk010@gmail.com>
2022-02-18 14:31:50 +0530

    updated

--- New.py
+++ New.py
@@ -3,10 +3,16 @@
 #print("hello python")
 
 #string
-a="sri lanka"
-print(a[2])
-print(a[0])
-print(a[4])
-print(a[1:5])
-print(a[2:])
-print(a*2)
+# a="sri lanka"
+# print(a[2])
+# print(a[0])
+# print(a[4])
+# print(a[1:5])
+# print(a[2:])
+# print(a*2)
+# print(a+"oshada eranga")
+
+#numbers
+# a=10
+# b=20
+# print(a+b)
